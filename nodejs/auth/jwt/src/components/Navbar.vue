494e376d848b08de59d94458268f832c7fe04eba
mestokaya <mesto1830@outlook.com>
2024-03-05 23:33:41 +0100

    bgbg

--- nodejs/auth/jwt/src/components/Navbar.vue
+++ nodejs/auth/jwt/src/components/Navbar.vue
@@ -4,11 +4,27 @@
       <router-link to="/" class="links">Home</router-link>
       <router-link to="/login" class="links">Login</router-link>
       <router-link to="/register" class="links">Register</router-link>
-      <router-link to="/logout" class="links">Logout</router-link>
+      <router-link to="" class="links" @click="logout()">Logout</router-link>
     </nav>
   </div>
 </template>
 
+<script>
+ export default {
+  name:'Navbar',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    logout(){
+      localStorage.removeItem('token')
+      this.$router.push('/login')
+    }
+  },
+ }
+</script>
 <style scoped>
   nav{
     display: flex;
