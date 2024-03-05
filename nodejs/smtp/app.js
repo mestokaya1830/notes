3df24886092d2b996e3f7efbfc3258a17a6a77d5
mestokaya <mesto1830@outlook.com>
2024-03-06 00:07:34 +0100

    fvf

---
+++ nodejs/smtp/app.js
@@ -0,0 +1,38 @@
+import express from 'express'
+const app = express()
+import nodeMailer  from 'nodemailer'
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+const transporter = nodeMailer.createTransport({
+  host: "smtp-mail.outlook.com",
+  auth: {
+    user:'mesto1830@outlook.com',
+    pass: 'MK1972mk11130113'
+  }
+})
+
+const mailoption  = {
+  from: 'mesto1830@outlook.com',
+  to: 'mesto1830@gmail.com',
+  //to: ['email1','email2','email3'],//for multiple receiver
+  subject: 'Test',
+  text: 'Hello Mesto',
+  html: `
+  <h3>How are you?To confirm your E-Mail please click on this link</h3>
+  <a href="https://www.mesfor.com">Activate</a>
+  `
+}
+
+transporter.sendMail(mailoption,(err, info) =>{
+  if (err) {
+    console.log(err)
+  }
+  console.log('Email send:' + info.response)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started...')
+})
