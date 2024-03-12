dbdaafb97b9d96183a79a2402be5d657aa0b7e80
mestokaya <mesto1830@outlook.com>
2024-03-12 01:00:51 +0100

    kik

---
+++ nodejs/auth/session/session-redisstore/app.js
@@ -0,0 +1,66 @@
+import express from 'express'
+const app = express()
+import path from 'path'
+import session from 'express-session';
+import RedisStore from "connect-redis"
+import {createClient} from "redis"
+let redisClient = createClient()
+redisClient.connect().catch(console.error)
+// import Redis from 'ioredis'
+// const redisClient = new Redis()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//session middleware
+app.use(session({
+  secret: 'admin',
+  resave: true,
+  saveUninitialized: false,//delete cookie on browser when expired
+  cookie: { maxAge: 1 * 60000},//1 minute
+  //cookie: { maxAge: 1 * 3600000},//1 hour
+  // cookie: { maxAge: 24 * 3600000},//1 day
+  rolling: true,//refresh cookie-time when user reactivate with server
+  httpOnly: true,//only transmit cookie over https
+  secure: true,//prevents client side js reading the cookies
+  store: new RedisStore({
+    client: redisClient
+  })
+}))
+
+//go index page
+  app.get('/',  (req, res) => {
+    res.sendFile(path.resolve('./index.html'))
+  })
+//go login page
+  app.get('/login',  (req, res) => {
+    res.sendFile(path.resolve('./login.html'))
+  })
+//post check login
+  app.post('/login',  (req, res) => {
+    if (req.body.username == 'mesto' && req.body.password == '9090') {
+      req.session.auth = req.body.username//set sesstion as auth
+      res.redirect('/admin')
+    } else {
+      res.redirect('/login')
+    }
+  })
+//check admin
+  app.get('/admin',(req, res) => {
+    console.log(req.session)
+    if(req.session.auth){
+      res.sendFile(path.resolve('./admin.html'))
+    }else{
+      res.redirect('/login')
+    }
+  })
+//logout
+  app.get('/logout',  (req, res) => {
+    req.session.destroy((err) => {
+      res.redirect('/')
+    })
+  })
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...')
+})
