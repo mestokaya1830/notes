d51052d201656bf8c6f7c4d95b08bc37870b22a5
mestokaya <mesto1830@outlook.com>
2024-04-04 03:16:50 +0200

    dcd

--- react/router/src/App.js
+++ react/router/src/App.js
@@ -1,8 +1,12 @@
 import { Router } from './router/index.js'
-
+import { Navbar } from './components/navbar.js';
+import './App.css'
 function App() {
   return (
+    <>
+    <Navbar />
     <Router />
+    </>
   );
 }
 
