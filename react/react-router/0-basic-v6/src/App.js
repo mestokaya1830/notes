89892eb43a5bf12d900da98bad8b50ff39b62e1b
mestokaya <mesto1830@outlook.com>
2024-04-11 03:15:58 +0200

    vfvfv

---
+++ react/react-router/0-basic-v6/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Router from './router/Index'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Router />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-router/0-basic-v6/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+function Navbar() {
+  return (
+    <div className='link-con'>
+      <NavLink to="/" className="link">Home</NavLink>
+      <NavLink to="/about" className="link">About</NavLink>
+      <NavLink to="/contact" className="link">Contact</NavLink>
+    </div>
+  )
+}
+
+export default Navbar
