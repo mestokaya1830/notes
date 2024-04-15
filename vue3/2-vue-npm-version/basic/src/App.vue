8e81e7af1cf64427aac18d893c796a67dcd648cc
mestokaya <mesto1830@outlook.com>
2024-04-15 16:25:15 +0200

    vfvfv

---
+++ vue3/2-vue-npm-version/basic/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Navbar class="navbar"/>
+  <RouterView class="router"/>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components:{Navbar},
+  setup () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ vue3/2-vue-npm-version/basic/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/" class="links" exact-active-class="active">Home</RouterLink>
+    </nav>
+  </div>
+</template>
