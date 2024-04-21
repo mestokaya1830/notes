84f6b8e6423507a17f347cbcee61d4f619cd6ab8
mestokaya <mesto1830@outlook.com>
2024-04-21 23:50:28 +0200

    dde

---
+++ vue3/nuxtjs/pages/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+        <NuxtLink to="/admin">Users</NuxtLink>
+        <NuxtLink to="/admin/settings">Settings</NuxtLink>
+      </nav>
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ vue3/nuxtjs/pages/admin/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+  const route = useRoute()
+  console.log(route)
+</script>
+<template>
+  <div>
+    <h2>Profile</h2>
+    <p>{{ $route.params.id}}</p>
+  </div>
+</template>
