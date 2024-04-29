1cb12b77f3acebc3c11408a932c294a9e4d23ac9
mestokaya <mesto1830@outlook.com>
2024-04-29 19:51:50 +0200

    rfrf

--- vue3/vue-pagination/app.js
+++ vue3/vue-pagination/app.js
@@ -2,6 +2,7 @@ import express  from 'express'
 const app = express()
 import mongoose from 'mongoose'
 import db from './modules/db.js'
+// import mysql from './modules/db-mysql.js'
 import cors from 'cors'
 import helmet from 'helmet'
 
@@ -14,16 +15,26 @@ const personsSchema = new mongoose.Schema({} , {strict: false})
 const Persons = new mongoose.model('Persons', personsSchema)
 
 app.post('/', async(req, res) => {
-  let page = req.body.page === '' ? 1 : req.body.page
-  let limit = 50
+  let page = req.body.page === undefined ? 1 : req.body.page
+  let limit = 5
+
+  //mongo
   const result = await Persons.find({},{_id:0}).limit(limit).skip((page-1) * limit)
   const length = await Persons.aggregate([{$count:'count'}])
-  res.json({result:result,length:length})
+  res.json({result,length})
+
+
+  //mysql
+  // mysql.query('SELECT * FROM persons WHERE ID > ? ORDER BY ID Limit ?',[page * limit, limit], (err, result) => {
+  //   mysql.query('SELECT COUNT(*) AS count FROM persons', (err, length) => {
+  //     res.json({result, length})
+  //   })
+  // })
 })
 
 app.use((err, req, res, next) => {
   console.log(err)
-  next()
+  next(err)
 })
 
 app.listen(3000, () => {
--- vue3/vue-pagination/src/views/Home.vue
+++ vue3/vue-pagination/src/views/Home.vue
@@ -2,11 +2,9 @@
   <div class="container">
     <h2>Vue Pagination</h2>
     <ul class="pagination">
-      <li v-for="(page, index) in parseInt(pages)" :key="index">
-        <span @click="getNext(page)">{{ page }}</span>
-      </li>
     </ul>
-    <table>
+    <!-- mongodb -->
+    <!-- <table>
       <thead>
         <tr class="table-header">
           <th class="table-header-item">ID</th>
@@ -21,10 +19,25 @@
         <td class="table-list-item">{{ item.gender }}</td>
         <td class="table-list-item">{{ item.company.location.country }}</td>
       </tr>
+    </table> -->
+    <!-- mysql -->
+    <table>
+      <thead>
+        <tr class="table-header">
+          <th class="table-header-item">ID</th>
+          <th class="table-header-item">Name</th>
+          <th class="table-header-item">Age</th>
+        </tr>
+      </thead>
+      <tr class="table-list" v-for="(item, index) in persons" :key="index">
+        <td class="table-list-item">{{ item.index }}</td>
+        <td class="table-list-item">{{ item.name }}</td>
+        <td class="table-list-item">{{ item.age }}</td>
+      </tr>
     </table>
     <ul class="pagination">
-      <li v-for="(page, index) in parseInt(pages)" :key="index">
-        <span @click="getNext(page)">{{ page }}</span>
+      <li v-for="(item, index) in parseInt(pages)" :key="index">
+        <span @click="nextPage(item)">{{ item }}</span>
       </li>
     </ul>
   </div>
@@ -41,20 +54,20 @@ export default {
     };
   },
   mounted() {
-    this.connectNodejs();
+    this.getPersons();
   },
   methods: {
-    async connectNodejs() {
-      await axios.post("http://localhost:3000").then((result) => {
+    async getPersons() {
+      await axios.post("http://localhost:3000/").then((result) => {
         this.persons = result.data.result;
-        this.pages = result.data.length[0].count / 50
-      });
+        this.pages = result.data.length[0].count / 5
+      })
     },
-    async getNext(page = 1) {
-      await axios.post("http://localhost:3000", { page }).then((result) => {
+    async nextPage(page = 1) {
+      await axios.post("http://localhost:3000/" , {page}).then((result) => {
         this.persons = result.data.result;
-        window.scrollTo(0, 0);
-      });
+        window.scrollTo(0, 0)
+      })
     }
   }
 };
@@ -68,9 +81,13 @@ export default {
   flex-direction: column;
   text-align: center;
 }
+.pagination{margin-top: 30px;}
+.pagination li{
+  margin: 0 10px;
+}
 .pagination li span{
   display: inline-block;
-  min-width: 20px;
+  min-width: 30px;
   background-color: #ddd;
   padding: 3px 6px;
   border-radius: 3px;
