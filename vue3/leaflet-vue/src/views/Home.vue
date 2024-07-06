1f62fc5951fff07fc031814d184162860781eec1
mestokaya1830 <mesto1830@outlook.com>
2024-07-06 02:01:00 +0200

    cd

---
+++ vue3/leaflet-vue/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+export default {
+  name: 'Home',
+  data() {
+    return {
+      title: 'Home Page',
+    }
+  },
+  mounted() {
+    this.getMap()
+  },
+  methods: {
+    getMap() {
+      let map = L.map('map').setView([51.505, -0.09], 13);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
+
+      let circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(map);
+
+    }
+  }
+};
+</script>
