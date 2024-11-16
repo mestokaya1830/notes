c4b4fdf10c982fe0d34f344906983f9ef10f9d7b
mestokaya1830 <mesto1830@outllok.com>
2024-11-16 17:48:29 +0100

    cdcd

--- react-mobil/app/index.js
+++ react-mobil/app/index.js
@@ -1,40 +1,105 @@
-import {View, ScrollView, StatusBar, Text, Button , StyleSheet, Image, ImageBackground} from 'react-native';
-const logo = require('../assets/images/react-logo.png')
+import { useState } from "react";
+import {
+  View,
+  ScrollView,
+  StatusBar,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Pressable,
+  Modal,
+} from "react-native";
+const logo = require("../assets/images/react-logo.png");
 
 export default function Home() {
+  const [isModal, setModel] = useState(false);
   return (
     <View style={styles.viewStyle}>
       <ScrollView>
         {/* <StatusBar style="auto" /> */}
         <Text style={styles.textStyle}>Welcome To React</Text>
-        <Image  source={logo} />
-        <Image  source={{uri : "https://picsum.photos/200/300"}} width={380} height={300} />
+        <Image source={logo} />
+        <Image
+          source={{ uri: "https://picsum.photos/200/300" }}
+          width={380}
+          height={300}
+        />
         {/* <View style={styles.imageCon}>
           <ImageBackground source={logo} resizeMethod='cover' style={styles.imageCon}>
             <Text style = {styles.textStyle}>Welcome To React</Text>
           </ImageBackground>
         </View> */}
-        
-        <Text style={{color: '#fff', padding: 20}}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+        <Text style={{ color: "#fff", padding: 20 }}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of
+          the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it to make a type specimen
+          book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum. Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
         </Text>
-        <Button title="Click" onPress={() => alert('Hello')}/>
+        <Button title="Button" onPress={() => alert("Hello")} />
+        <Pressable onPress={() => alert("Pressable Area")}>
+          <Text style={styles.textStyle2}>Click Here (Pressable)</Text>
+        </Pressable>
+
+        <View style={{ marginBottom: 20, marginTop: 20 }}>
+          <Button title="Open Model" onPress={() => setModel(true)} />
+        </View>
+        <Modal visible={isModal} animationType="fade">
+          <View style={styles.modalStyle}>
+            <Text style={{ fontSize: 30 }}>Modal</Text>
+          </View>
+          <Button title="Close" onPress={() => setModel(false)} />
+        </Modal>
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  viewStyle:{
+  viewStyle: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'purple'
+    alignItems: "center",
+    backgroundColor: "purple",
   },
-  textStyle:{
+  textStyle: {
     fontSize: 30,
-    color: '#fff',
-    fontWeight: 'bold'
-  }
-})
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  textStyle2: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  modalStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
