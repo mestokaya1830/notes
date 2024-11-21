6f428123423ca3954157de167f28e11f640a3eaa
mestokaya1830 <mesto1830@outllok.com>
2024-11-21 10:14:03 +0100

    vfvf

--- react-mobil/app/components/message.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native"
-
-export default function Message() {
-  return (
-    <View>
-      <Text style={{fontSize: 30, color:"#fff"}}>Custom Component</Text>
-    </View>
-  )
-}
