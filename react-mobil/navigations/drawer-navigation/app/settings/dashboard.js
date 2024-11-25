e2c66b1c3f6d8a985d7c4d25c7c9d3250741754d
mestokaya1830 <mesto1830@outllok.com>
2024-11-25 16:59:33 +0100

    vfvf

---
+++ react-mobil/navigations/drawer-navigation/app/settings/dashboard.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Dashboard() {
+  return (
+    <View>
+      <Text>Dashboard Details</Text>
+    </View>
+  )
+}
---
+++ react-mobil/navigations/drawer-navigation/app/settings/users.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Users() {
+  return (
+    <View>
+      <Text>Users Details</Text>
+    </View>
+  )
+}
