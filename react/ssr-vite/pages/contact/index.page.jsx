1e4537cdcc09980a2898aef674a5142bd7b8e30e
mestokaya1830 <mesto1830@outllok.com>
2024-12-31 17:02:22 +0100

    xxs

--- react/ssr-vite/pages/contact/index.page.jsx
+++
@@ -1,9 +0,0 @@
-
-export { Page }
-function Page() {
-  return (
-    <>
-      <h1>Contact</h1>
-    </>
-  )
-}
--- vue-nuxt/basic/pages/users/[id]/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  const {id} = useRoute().params
-</script>
-<template>
-  <div>
-    <h1>User Details</h1>
-    <h2>{{ id }}</h2>
-  </div>
-</template>
