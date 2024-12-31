4aa0e844ac83fbe27d8b156150361527096d5542
mestokaya1830 <mesto1830@outllok.com>
2025-01-01 00:22:57 +0100

    xsxs

---
+++ vue-nuxt/basic/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import Navbar from '../compnents/navbar'
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ vue-nuxt/basic/pages/admin/users/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+  definePageMeta({
+    layout: "admin",
+  });
+  const { data: users } = await useFetch("https://jsonplaceholder.typicode.com/users")
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="item in users" :key="item.id">
+        <span>{{ item.id }}</span>
+        <span>{{ item.name }}</span>
+        <span>{{ item.email }}</span>
+        <NuxtLink :to="`/admin/users/${item.id}`">Details</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
