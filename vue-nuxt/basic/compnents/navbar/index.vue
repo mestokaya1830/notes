830b248d0242710877c755ea143ee63a52ce823d
mestokaya1830 <mesto1830@outllok.com>
2025-01-01 18:53:40 +0100

    edede

--- vue-nuxt/basic/compnents/navbar/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <NuxtLink to="/" class="links" exact-active-class="active">Home</NuxtLink>
-    <NuxtLink to="/clients/about" class="links" exact-active-class="active">About</NuxtLink>
-    <NuxtLink to="/clients/contact" class="links" exact-active-class="active">Contact</NuxtLink>
-    <NuxtLink to="/admin/users" class="links" exact-active-class="active">Users</NuxtLink>
-  </div>
-</template>
