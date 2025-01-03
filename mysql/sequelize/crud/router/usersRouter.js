626250a59651bc16d25cc43a98541954267d6f51
mestokaya1830 <mesto1830@outllok.com>
2025-01-03 17:20:55 +0100

    mkmkm

--- mysql/sequelize/crud/router/usersRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import Users from '../controller/Users.js';
-
-router.post('/adduser', Users.addUser)
-router.post('/removeuser/:id', Users.removeUser)
-
-export default router
