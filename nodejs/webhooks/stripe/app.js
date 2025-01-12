013d1ded9b5d93186ce7228b6ce0350128319736
mestokaya1830 <mesto1830@outllok.com>
2025-01-12 23:12:56 +0100

    cdcd

--- nodejs/webhooks/stripe/app.js
+++ nodejs/webhooks/stripe/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import pg from 'pug'
+import expressLayouts from 'express-ejs-layouts'
 const app = express()
 import cors from 'cors'
 import stripe from 'stripe'
@@ -8,8 +8,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static('static'))
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
 
-app.set('view engine','pug')
 
 let strp = stripe('sk_test_51KsAmgGjc2DBcMZIJwlMFs6s8gQQOxvN9mXqEYbZFkFHZg339AJY9SOhkH7rmmQL9ePqak1tANpSDIDMi7lkqNYp00rO853BbL')
 
@@ -19,17 +20,19 @@ app.get('/', (req, res) => {
     key:'pk_test_51KsAmgGjc2DBcMZI8vlI0imznOZUw5lR5AMdAhKCUMl8QquJPVnb6EWC5TELup7x3terZUEj4A9Wim6Vps25m1Ki00uPSjRrhE'
   })
 })
+
 app.post('/payment', (req, res) => {
+  const {stripeEmail, stripeToken, name, postal_code, city, state, country, address} = req.body
   strp.customers.create({
-    email:req.body.stripeEmail,
-    source:req.body.stripeToken,
-    name:'Mesto Kaya',
+    email:stripeEmail,
+    source:stripeToken,
+    name:name,
     address: {
-      line1:'address',
-      postal_code:8000,
-      city:'Zurich',
-      state:'Zurich',
-      country:'Switzerland'
+      line1:address,
+      postal_code:postal_code,
+      city:city,
+      state:state,
+      country:country
     }
   }).then(customer => {
     return strp.charges.create({
@@ -41,6 +44,13 @@ app.post('/payment', (req, res) => {
   }).then(charge => {
     res.send('Success')
   }).catch(error => console.log(error))
+})
+
+
+//get all customers from this account
+app.get('/customers', async (req, res) => {
+  const customers = await strp.customers.list({}, {stripeAccount: 'acct_1KsAmgGjc2DBcMZI'})
+  res.send(customers)
 })
 
 
