a8233a060ddf29e56a008ccc41a61f7e04e4ad7b
mestokaya1830 <mesto1830@outllok.com>
2025-01-18 18:30:11 +0100

    ccdcd

--- javascript/breadcrumbs/breadcrumbs.js
+++ javascript/breadcrumbs/breadcrumbs.js
@@ -6,6 +6,7 @@ links.forEach((item, index) => {
   item.addEventListener('click', (e) => {
     crumbsList = []
     for (let i = 0; i <= index; i++) {
+      console.log(routes[i])
       crumbsList.push(`<a href="/${routes[i]}" class="active">${routes[i]}</a>`)
     }
     document.getElementById('crumbs').innerHTML = crumbsList.map(item => item.split(',')).join(' > ')
