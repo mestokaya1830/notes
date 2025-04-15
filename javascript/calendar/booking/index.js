e493e971d6a1736f832e93ef616df4e54d9a53bc
mestokaya1830 <mesto1830@outllok.com>
2025-04-16 00:38:24 +0200

    xxsx

--- javascript/calendar/booking/index.js
+++ javascript/calendar/booking/index.js
@@ -8,15 +8,21 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
-let nav = 0
+let monthNumber = 0
 
 const load = () => {
   const dt = new Date();
+
+  //get months for header's month not(this must be here above month variable)
+  if (monthNumber !== 0) {
+    dt.setMonth(new Date().getMonth() + monthNumber);
+  }
 
   //get day,month,year as digit
   const day = dt.getDate();
   const month = dt.getMonth();
   const year = dt.getFullYear();
+
 
   //set current month in header as string
   document.getElementById("current-month").innerText = dt.toLocaleDateString("en-EN",{ month: "long" }) +' '+ year;
@@ -28,13 +34,6 @@ const load = () => {
     month: "numeric",
     day: "numeric",
   });
-
-  
-
-  //get months
-  if (nav !== 0) {
-    dt.setMonth(new Date().getMonth() + nav);
-  }
 
 
   //get the index of firstDayOfMonth in weekdays to calculate empty days in calendar
@@ -54,11 +53,20 @@ const load = () => {
       days.innerText = i - paddingDays;
 
       //get current day
-      if (i - paddingDays === day && nav === 0) {
+      if (i - paddingDays === day && monthNumber === 0) {
         days.id = "current-day";
       }
-  
-      // const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+      
+      //open event panel on calendar
+      const dates = `${month + 1}/${i - paddingDays}/${year}`;
+      days.addEventListener('click', () => {
+        document.getElementById('event-panel').style.display = 'block'
+      })
+      document.getElementById('eventCloseBtn').addEventListener('click', () => {
+        document.getElementById('event-panel').style.display = 'none'
+      })
+      
+      // let events = localStorage.getItem("events") ? JSON.parse(localStorage.getItem("events")) : [];
       // const addEventDay = events.find((item) => item.date === dayString);
     } else {
       // days.classList.add("paddings");
@@ -67,4 +75,12 @@ const load = () => {
   }
 }
 
+document.getElementById("back").addEventListener("click", (e) => {
+  monthNumber--;
+  load();
+});
+document.getElementById("next").addEventListener("click", (e) => {
+  monthNumber++;
+  load();
+});
 load()
